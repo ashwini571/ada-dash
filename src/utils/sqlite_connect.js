323c782bce67aed23483a713e4fbb1ee22ad3bad
ashwini571 <ojha.ashwini.1998@gmail.com>
2020-05-16 16:03:00 +0530

    sqlite connection added

---
+++ src/utils/sqlite_connect.js
@@ -0,0 +1,9 @@
+// execution mode is set to verbose to produce long stack traces
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('sqlite_db.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err)
+        console.log(err.message);
+    else
+        console.log('Connected to the sqlite database.');
+});
