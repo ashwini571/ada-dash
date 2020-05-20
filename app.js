a07b2c1237cff8945ee1bdddd6f66cfb31199f24
ashwini571 <ojha.ashwini.1998@gmail.com>
2020-05-20 15:26:48 +0530

    One can see all queries of a usecase(Add/Edit) + hbs helper added for ifCondition in template + Alert Box added in UI for errors/Messages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let logger = require('morgan')
 let indexRouter = require('./routes/index')
 let analyticsRouter = require('./routes/analytics')
+let allQueriesRouter = require('./routes/all_queries')
 let AWS = require('aws-sdk')
 let express = require('express')
 let createError = require('http-errors')
@@ -11,19 +12,48 @@ let app = express()
 //template engine
 let hbs = require('hbs');
 hbs.registerPartials(__dirname + '/views/partials')
-
-app.use(logger('dev'))
-app.use(express.json())
+hbs.registerHelper('ifCond', function (v1, operator, v2, options) {
+  switch (operator) {
+    case '==':
+      return (v1 == v2) ? options.fn(this) : options.inverse(this);
+    case '===':
+      return (v1 === v2) ? options.fn(this) : options.inverse(this);
+    case '!==':
+      return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+    case '<':
+      return (v1 < v2) ? options.fn(this) : options.inverse(this);
+    case '<=':
+      return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+    case '>':
+      return (v1 > v2) ? options.fn(this) : options.inverse(this);
+    case '>=':
+      return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+    case '&&':
+      return (v1 && v2) ? options.fn(this) : options.inverse(this);
+    case '||':
+      return (v1 || v2) ? options.fn(this) : options.inverse(this);
+    default:
+      return options.inverse(this);
+  }
+});
 
 // view engine setup
 app.set('view engine', 'hbs')
 app.set('views',__dirname + '/views')
 
+app.use(logger('dev'))
+app.use(express.json())
+
+
+
+
 app.use('/static', express.static(__dirname + '/node_modules/'));
 app.use('/public', express.static(__dirname + '/public/'));
+
 //Route files
 app.use('/', indexRouter)
 app.use('/analytics', analyticsRouter)
+app.use('/all_queries', allQueriesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/all_queries.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const router = express.Router()
+const redshift = require('../src/utils/redshift_connect')
+const sqliteDb = require('../src/utils/sqlite_connect')
+
+/* Middleware for getting POST body data */
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const redshiftClient = redshift.redshiftClient
+const clusterName = redshift.clusterName
+
+router.get('/:id', (req, res )=> {
+
+    /* fetching all cases from sqlite */
+    let sql = `SELECT * FROM all_queries WHERE usecase_id = '${req.params.id}' `
+    sqliteDb.all(sql,[],(err,row)=>{
+        /* row.length==0 so that it doesn't catch error in  row[0].title*/
+        if(err || row.length==0)
+            return res.render('templates/all_queries',{title:"Error",error:"Error querying Sqlite"})
+
+        res.render('templates/all_queries', { title:"All Queries", clusterName:clusterName,result:row })
+
+    })
+
+})
+
+router.get('/add_filter/:id', (req,res)=>{
+    res.render('templates/add_filter')
+})
+
+router.post('/add_filter/:id', (req,res)=>{
+    res.render('templates/add_filter')
+})
+
+module.exports = router
